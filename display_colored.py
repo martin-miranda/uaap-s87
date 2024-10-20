cbd9d7ada6e6a92c75f3cc600b9dc190ef8a440c
Martin Miranda <carl_miranda@dlsu.edu.ph>
2024-10-20 13:56:35 +0800

    Fixed SP. Added Opponent Stats tab.

--- display_colored.py
+++ display_colored.py
@@ -28,9 +28,9 @@ with open('title.txt', 'r') as file:
 st.title(title[0])
 st.markdown('By Carl Miranda @mc_miranda34')
 st.markdown('Raw Box Scores from Pong Ducanes: uaap.livestats.ph')
-st.markdown('#### As of Game 36 - AdU (Oct. 19)')
+st.markdown('#### As of Game 36 - AdU vs. DLSU (Oct. 19)')
 
-tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(['Player Per-Game Stats', 'Player Per-30 Minute Stats', 'Player Advanced Stats', 'Team Per-Game Stats', 'Team Advanced Stats', 'Glossary'])
+tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(['Player Per-Game Stats', 'Player Per-30 Minute Stats', 'Player Advanced Stats', 'Team Per-Game Stats', 'Opponent Per-Game Stats', 'Team Advanced Stats', 'Glossary'])
 cm = sns.dark_palette("green", as_cmap=True)
 
 with tab1:
@@ -68,7 +68,7 @@ with tab2:
         st.write(df)
 
 with tab3:
-    st.markdown('*Please note the discrepancies on the SP calculation at the bottom of the page.*')
+    st.markdown('*Note for SP: Penalties are not included due to inavailability of data.*')
     st.header('All Players', divider='gray')
     df = pd.read_csv('advanced_stats.csv', index_col=['PLAYER','TEAM'])
     df = df[(df['MPG'] * df['GP']) >= df['GP'].max() * 8]
@@ -85,12 +85,6 @@ with tab3:
         df = df.style.background_gradient(cmap=tcm, axis=0).format("{:.2f}")
         st.write(df)
 
-    st.write('---')
-    st.markdown('There are slight discrepancies on the official SP calculations due to the bonus and penalties in the formula. This would not probably affect **rankings** unless adjacent players have very close SP.')
-    st.markdown('The discrepancies are as follows:')
-    st.markdown('1. Win bonuses are counted by team instead of by player for simplicity in coding. *For example, the missed win by JD Cagulangan is still included in his bonus.*')
-    st.markdown('2. Penalties are not included at all due to lack of availability on technical and unsportsmanlike foul data.')
-
 with tab4:
     st.header('All Teams', divider='gray')
     df = pd.read_csv('team_per_game.csv', index_col=['TEAM'])
@@ -101,12 +95,20 @@ with tab4:
 
 with tab5:
     st.header('All Teams', divider='gray')
+    df = pd.read_csv('opp_per_game.csv', index_col=['TEAM'])
+    df = df.reindex(columns=tb_cols)
+    df = df.drop(labels=['W','L','W%','MINS'], axis=1)
+    df = df.style.background_gradient(cmap=cm, axis=0).format("{:.2f}")
+    st.write(df)
+
+with tab6:
+    st.header('All Teams', divider='gray')
     df = pd.read_csv('team_advanced.csv', index_col=['TEAM'])
     df = df.reindex(columns=ta_cols)
     df = df.style.background_gradient(cmap=cm, axis=0).format("{:.2f}")
     st.write(df)
 
-with tab6:
+with tab7:
     with open('Glossary.md','r') as f:
         markdown_content = f.read()
         st.markdown(markdown_content)
